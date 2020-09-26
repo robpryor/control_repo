30e8c8a48e38241e56ad44bde31ec19d3cbff4c3
robpryor <71915627+robpryor@users.noreply.github.com>
2020-09-26 04:17:09 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBjnrfcd3ZDa5j/9nA7ounf0WBI4adF80j53NvjZoifKG6TjP5D86REXSqLRyN8aSxWyFt3LO7FaPTY7/17sL9SLgS8ptPrttIf5gEhkD/EujzK9QIefEVkMFBsswIe0jm7Ih5+zM7ODHej3INalf1DYchMY8Os5y5eDM5MKeyGC9XLPS4kth9Em+p0uOIq+mmByLja5UmXRc/tI6lhWzOvQNdUct3HjYScK/nuKwgd+D8OFq+zt5vZDnk3Dtp0djfYNjJPp1ydkrlHgWVQBlL5nfnWBH9y0I5K1ubv8ulhPRv7A7EMqT0J/0Ar2RjLp11X0nAC93KSdD8PX83vuIX',
   }  
 }
