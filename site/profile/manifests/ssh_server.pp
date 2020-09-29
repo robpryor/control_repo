7004a97c4353c0ee0b68e20857c0b231eb5c71e5
robpryor <71915627+robpryor@users.noreply.github.com>
2020-09-29 02:43:53 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,15 +1,15 @@
-class profile::ssh_server {
-  package {'openssh-server':
-    ensure => present,
-  }
-  service { 'sshd':
-    ensure => 'running',
-    enable => 'true',
-  }
-  ssh_authorized_key { 'root@master.puppet.vm':
-    ensure => present,
-    user   => 'root',
-    type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBjnrfcd3ZDa5j/9nA7ounf0WBI4adF80j53NvjZoifKG6TjP5D86REXSqLRyN8aSxWyFt3LO7FaPTY7/17sL9SLgS8ptPrttIf5gEhkD/EujzK9QIefEVkMFBsswIe0jm7Ih5+zM7ODHej3INalf1DYchMY8Os5y5eDM5MKeyGC9XLPS4kth9Em+p0uOIq+mmByLja5UmXRc/tI6lhWzOvQNdUct3HjYScK/nuKwgd+D8OFq+zt5vZDnk3Dtp0djfYNjJPp1ydkrlHgWVQBlL5nfnWBH9y0I5K1ubv8ulhPRv7A7EMqT0J/0Ar2RjLp11X0nAC93KSdD8PX83vuIX',
-  }  
-}
+#class profile::ssh_server {
+#  package {'openssh-server':
+#    ensure => present,
+#  }
+#  service { 'sshd':
+#    ensure => 'running',
+#    enable => 'true',
+#  }
+#  ssh_authorized_key { 'root@master.puppet.vm':
+#    ensure => present,
+#    user   => 'root',
+#    type   => 'ssh-rsa',
+#    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBjnrfcd3ZDa5j/9nA7ounf0WBI4adF80j53NvjZoifKG6TjP5D86REXSqLRyN8aSxWyFt3LO7FaPTY7/17sL9SLgS8ptPrttIf5gEhkD/EujzK9QIefEVkMFBsswIe0jm7Ih5+zM7ODHej3INalf1DYchMY8Os5y5eDM5MKeyGC9XLPS4kth9Em+p0uOIq+mmByLja5UmXRc/tI6lhWzOvQNdUct3HjYScK/nuKwgd+D8OFq+zt5vZDnk3Dtp0djfYNjJPp1ydkrlHgWVQBlL5nfnWBH9y0I5K1ubv8ulhPRv7A7EMqT0J/0Ar2RjLp11X0nAC93KSdD8PX83vuIX',
+#  }  
+#}
