7800bb24b744917fc5687709862d6718a764296f
robpryor <71915627+robpryor@users.noreply.github.com>
2020-09-29 03:07:25 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -5,14 +5,14 @@ class profile::agent_nodes {
   dockeragent::node { 'minetest.puppet.vm': }
   host {'web.puppet.vm':
     ensure => present,
-    ip => 192.168.1.201
+    ip => 192.168.1.201,
   }
   host {'db.puppet.vm':
     ensure => present,
-    ip => 192.168.1.202
+    ip => 192.168.1.202,
   }
    host {'minetest.puppet.vm':
     ensure => present,
-    ip => 192.168.1.203
+    ip => 192.168.1.203,
   }
 }
