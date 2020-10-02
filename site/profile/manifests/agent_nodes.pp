037e6851826bda813e82c7a78d8a74e7f37db126
robpryor <71915627+robpryor@users.noreply.github.com>
2020-10-02 02:27:36 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,6 @@
 class profile::agent_nodes {
   include 'docker'
-  docker::node { 'web.puppet.vm': }
+  docker::node_update { 'web.puppet.vm': }
   docker::node { 'db.puppet.vm': }
   docker::node { 'minetest.puppet.vm': }
   host {'web.puppet.vm':
